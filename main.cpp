b77ac56a78f2f9921ea219c5790dc10da50d1ab6
snir2g2 <snir2g2@localhost>
2018-01-16 18:35:15 +0100

    Cout coucou

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 16 janvier 2018, 18:33
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "COUCOUUUU" << endl;
+    return 0;
+}
+
